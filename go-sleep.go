743ab5f1bb487edf1772bc29ca0bdf572b40785e
Christian Couder <chriscool@tuxfamily.org>
2015-06-06 22:40:34 +0200

    Misc output fixes

--- go-sleep.go
+++ go-sleep.go
@@ -12,7 +12,7 @@ func main() {
 	}
 	d, err := time.ParseDuration(os.Args[1])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "could not parse duration: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Could not parse duration: %s\n", err)
 		usageError()
 	}
 
@@ -22,6 +22,6 @@ func main() {
 func usageError() {
 	fmt.Fprintf(os.Stderr, "Usage: %s <duration>\n", os.Args[0])
 	fmt.Fprintln(os.Stderr, `Valid time units are "ns", "us" (or "µs"), "ms", "s", "m", "h".`)
-	fmt.Fprintf(os.Stderr, "See https://godoc.org/time#ParseDuration for more.")
+	fmt.Fprintln(os.Stderr, "See https://godoc.org/time#ParseDuration for more.")
 	os.Exit(-1)
 }
